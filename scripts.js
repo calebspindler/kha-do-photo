724147b249898176d0b5068f0dc1d2639adfdbc3
Caleb Spindler <caleb.spindler@gmail.com>
2018-09-20 18:01:44 -0400

    fixed link to home

--- scripts.js
+++ scripts.js
@@ -59,3 +59,15 @@ function loadContact() {
   xhttp.send();
   window.history.pushState("object or string", "Title", "/contact");
 }
+
+function loadIndex() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("photoGallery").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/kha-do-photo/index.html", true);
+  xhttp.send();
+  window.history.pushState("object or string", "Title", "/");
+}
