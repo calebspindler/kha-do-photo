289273dc7782b19790031d7590c77eddd2d2be31
Caleb Spindler <caleb.spindler@gmail.com>
2018-09-21 22:36:27 -0400

    removed AJAX soft-load and added new docs

--- scripts.js
+++ scripts.js
@@ -32,32 +32,32 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "flex";
 }
 
-/* ajax */
-
-// Load About Page
-function loadAbout() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("photoGallery").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/kha-do-photo/about.html", true); //Remove kha-do-photo before launch
-  // /kha-do-photo
-  xhttp.send();
-  // window.history.pushState("object or string", "Title", "/about");
-}
-
-// Load Contact Page
-function loadContact() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("photoGallery").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/kha-do-photo/contact.html", true);
-  // /kha-do-photo
-  xhttp.send();
-  // window.history.pushState("object or string", "Title", "/contact");
-}
+// /* ajax */
+//
+// // Load About Page
+// function loadAbout() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("photoGallery").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "/kha-do-photo/about.html", true); //Remove kha-do-photo before launch
+//   // /kha-do-photo
+//   xhttp.send();
+//   // window.history.pushState("object or string", "Title", "/about");
+// }
+//
+// // Load Contact Page
+// function loadContact() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("photoGallery").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "/kha-do-photo/contact.html", true);
+//   // /kha-do-photo
+//   xhttp.send();
+//   // window.history.pushState("object or string", "Title", "/contact");
+// }
